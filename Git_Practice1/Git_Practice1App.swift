2d12f0408554b1940392bc10b31f53ad5d60c1b5
Angelia Causing <aecausing@gmail.com>
2025-07-23 11:02:26 -0700

    Initial Commit

---
+++ Git_Practice1/Git_Practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Practice1App.swift
+//  Git_Practice1
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Git_Practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Git_Practice1Tests/Git_Practice1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Practice1Tests.swift
+//  Git_Practice1Tests
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import Testing
+@testable import Git_Practice1
+
+struct Git_Practice1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
